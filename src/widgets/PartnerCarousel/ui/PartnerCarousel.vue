5278f5ae7558f34e3d6f55dbbf476f7ce8e72233
DavidKaigorodov <Bossdilan2003@gmail.com>
2025-10-26 22:24:13 +0700

    сделал секцию с партнерами

---
+++ src/widgets/PartnerCarousel/ui/PartnerCarousel.vue
@@ -0,0 +1,29 @@
+<script>
+import Carousel from "@/shared/ui/carousel/Carousel.vue";
+import ImageCard from "@/shared/ui/imageCard/ImageCard.vue";
+
+export default {
+  components: { Carousel, ImageCard },
+  props: {
+    partners: { type: Array, required: true },
+  },
+};
+</script>
+
+<template>
+  <Carousel :items="partners" :visibleCount="3" :gap="20">
+    <template #default="{ item }">
+      <div class="carousel-item-wrapper">
+        <ImageCard :src="item.image" :alt="item.name" />
+      </div>
+    </template>
+  </Carousel>
+</template>
+
+<style lang="sass" scoped>
+.carousel-item-wrapper
+  width: 250px
+  height: 250px
+  position: relative
+  flex-shrink: 0
+</style>
---
+++ src/widgets/PartnerSection/ui/PartnerSection.vue
@@ -0,0 +1,26 @@
+<script>
+import { partners } from "@/entities/partners";
+import PartnerCarousel from "@/widgets/PartnerCarousel/ui/PartnerCarousel.vue";
+
+export default {
+  components: { PartnerCarousel },
+  data() {
+    return {
+      partners,
+    };
+  },
+};
+</script>
+<template>
+  <section class="partners-section">
+    <h1 class="partners-title">Наши партнёры</h1>
+    <PartnerCarousel :partners="partners" />
+  </section>
+</template>
+
+<style lang="sass" scoped>
+.partners-section
+  text-align: center
+  display: flex
+  justify-content: space-evenly
+</style>
