982fa6c8077f3ab85962c0670031f8c8a3821d55
DavidKaigorodov <Bossdilan2003@gmail.com>
2025-10-26 22:23:08 +0700

    сделал секцию с информацией

---
+++ src/entities/about/AboutCard.vue
@@ -0,0 +1,51 @@
+<script>
+import ImageCard from "@/shared/ui/imageCard/ImageCard.vue";
+
+export default {
+  components: { ImageCard },
+  props: {
+    image: { type: String, required: true },
+    title: { type: String, default: "" },
+    label: { type: String, default: "" },
+  },
+};
+</script>
+
+<template>
+  <div class="about-card">
+    <ImageCard :src="image" :alt="title" class="about-card__image" />
+    <p class="about-card__label">{{ label }}</p>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.about-card
+  display: flex
+  flex-direction: column
+  align-items: center
+  text-align: center
+
+  .about-card__image
+    width: 320px
+    height: 320px
+    border-radius: 50%
+    overflow: hidden
+    position: relative
+    transition: transform 0.3s ease
+    margin-bottom: 12px
+
+    &:hover
+      transform: scale(1.02)
+
+    .image-card__image
+      width: 100%
+      height: 100%
+      object-fit: cover
+      border-radius: 50%
+
+  .about-card__label
+    font-weight: 600
+    font-size: 20px
+    margin: 0
+    text-align: center
+</style>
---
+++ src/widgets/AboutSection/ui/AboutSection.vue
@@ -0,0 +1,54 @@
+<script>
+import { AboutCard, abouts } from "@/entities/about";
+export default {
+  components: {
+    AboutCard,
+  },
+  data() {
+    return { abouts };
+  },
+};
+</script>
+
+<template>
+  <section class="about-section">
+    <h1>
+      <span class="highlight">Ничего лишнего</span>, никаких консервантов
+      <br />
+      Только <span class="highlight">МЯСО</span>
+    </h1>
+    <div class="images-wrapper">
+      <AboutCard v-for="about in abouts" :key="about.id" v-bind="about" />
+    </div>
+
+    <span class="description">
+      Мы готовим мясо, которое говорит
+      <span class="highlight">само за себя</span><br />
+      Коптим, сушим и доводим до
+      <span class="highlight">идеального вкуса</span> - без спешки,
+      <span class="highlight">без химии</span>, с душой<br />
+      Мы не гонимся за объемом - делаем мясо,
+      <span class="highlight">которое не стыдно поставить на стол</span>
+    </span>
+  </section>
+</template>
+
+<style lang="sass" scoped>
+.about-section
+  text-align: center;
+  display: flex;
+  justify-content: space-evenly;
+
+  .images-wrapper
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+    justify-content: start;
+    justify-items: center;
+    gap: 40px;
+    margin: 40px 0;
+    width: 100%;
+
+  .description
+    font-size: 20px;
+    line-height: 1.6;
+</style>
